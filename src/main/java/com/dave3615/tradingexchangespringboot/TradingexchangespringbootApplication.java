eb4e717ec605d4b2780e4f1fe349dcc7e6ff1f3f
Julian Sagen <julian.sagen@live.no>
2018-04-05 11:52:04 +0200

    initial commit

---
+++ src/main/java/com/dave3615/tradingexchangespringboot/TradingexchangespringbootApplication.java
@@ -0,0 +1,12 @@
+package com.dave3615.tradingexchangespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingexchangespringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingexchangespringbootApplication.class, args);
+    }
+}
