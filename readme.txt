14307de5e8acb4ae45f26886d3556cf8b2f48fb4
Julian <teritry2@gmail.com>
2018-04-10 23:36:59 +0200

    Users can now see their acount balance

---
+++ readme.txt
@@ -0,0 +1,13 @@
+This will be all functionality for now.
+
+Functional spring security settup with roles for user, and admin(Only user being used).
+Users can logg inn and see their current bitcoin, and US dollar balance.
+
+More changes will come in the final version
+
+Github repo for this project: https://github.com/teritry/tradingexchangespringboot
+
+See canvas comment for online link to application hosted on AWS.
+
+
+-Julian
--- src/main/java/com/dave3615/tradingexchangespringboot/controller/MainController.java
+++ src/main/java/com/dave3615/tradingexchangespringboot/controller/MainController.java
@@ -1,10 +1,14 @@
 package com.dave3615.tradingexchangespringboot.controller;
 
+import com.dave3615.tradingexchangespringboot.dao.UserDAO;
 import com.dave3615.tradingexchangespringboot.model.User;
 import com.dave3615.tradingexchangespringboot.service.LoginService;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +20,9 @@ public class MainController {
 
         @Autowired
         LoginService loginService;
+
+        @Autowired
+        UserDAO userDAO;
 
         @GetMapping("/")
         public String home(Model model) {
@@ -39,7 +46,14 @@ public class MainController {
     }
 
     @GetMapping("/userpage")
+    @PreAuthorize("hasRole(@roles.USER)")
     public String userpage(Model model) {
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userDAO.findByUsername(((UserDetails)principal).getUsername());
+
+
+        model.addAttribute("user", user);
 
         System.out.println("Loading form");
         return "accountadministration";
